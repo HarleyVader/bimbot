4744e0a613fb32cb84c791aa0d0ae5d590280557
harleyvader <urukkai@outlook.com>
2022-08-12 04:50:03 +0200

    0.0.5

--- index.js
+++ index.js
@@ -5,19 +5,6 @@ const { token } = require('./config.json');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-const eventsPath = path.join(__dirname, 'events');
-const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
-
-for (const file of eventFiles) {
-    const filePath = path.join(eventsPath, file);
-    const event = require(filePath);
-    if (event.once) {
-        client.once(event.name, (...args) => event.execute(...args));
-    } else {
-        client.on(event.name, (...args) => event.execute(...args));
-    }
-}
-
 client.commands = new Collection();
 const commandsPath = path.join(__dirname, 'commands');
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
@@ -28,9 +15,9 @@ for (const file of commandFiles) {
     client.commands.set(command.data.name, command);
 }
 
-//client.once('ready', () => {
-//    console.log('Ready!');
-//});
+client.once('ready', () => {
+    console.log('Ready!');
+});
 
 client.on('interactionCreate', async interaction => {
     if (!interaction.isChatInputCommand()) return;
@@ -46,5 +33,3 @@ client.on('interactionCreate', async interaction => {
         await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
     }
 });
-
-client.login(token);
